681c1bb5fab12a5699367903c2632752eaf11726
Nurul Srianda Putri <nurulsriandaputri@gmail.com>
2022-04-13 08:52:30 +0700

    add congrats page

---
+++ SunnySideUp/CongratsViewController.swift
@@ -0,0 +1,34 @@
+//
+//  CongratsViewController.swift
+//  SunnySideUp
+//
+//  Created by Nurul Srianda Putri on 13/04/22.
+//
+
+import UIKit
+
+class CongratsViewController: ViewController {
+
+    @IBOutlet weak var CongratsTitleLabel: UILabel!
+    @IBOutlet weak var CongratsMessageLabel: UILabel!
+    @IBOutlet weak var CongratsMenuImageView: UIImageView!
+    @IBOutlet weak var doneBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
