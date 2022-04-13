a0dfe3f6ff355b0d80ed524f709682534969b40b
Nurul Srianda Putri <nurulsriandaputri@gmail.com>
2022-04-13 10:21:43 +0700

    update RecipeListViewController.swift

--- SunnySideUp/RecipeListViewController.swift
+++ SunnySideUp/RecipeListViewController.swift
@@ -4,38 +4,36 @@
 //
 //  Created by Nurul Srianda Putri on 11/04/22.
 //
-
 import UIKit
 
 class RecipeListViewController: ViewController {
     
-    var weeklyRecipes: [OneDayMenu] = [
-        OneDayMenu(dayNumber: 1,
-                   dayMenuList: [
-                    Menu(menuTitle: "Tempe Mendol", menuCookTime: "20 Mins", menuCalorie: "100 kcal", menuImageName: "tempe_mendol", menuDescription: "Deskripsi tempe mendol apa yaaa"),
-                    Menu(menuTitle: "Salted Egg Tempe", menuCookTime: "30 Mins", menuCalorie: "150 kcal", menuImageName: "tempe_salted_egg", menuDescription: "Wow salted egg tempe, enak sekali!!"),
-                    Menu(menuTitle: "Resep abc", menuCookTime: "30 Mins", menuCalorie: "150 kcal", menuImageName: "tempe_salted_egg", menuDescription: "tempe dengan kecap ABC ulala")
-                ]),
-        OneDayMenu(dayNumber: 2,
-                   dayMenuList: [
-                    Menu(menuTitle: "spongebob", menuCookTime: "20 Mins", menuCalorie: "100 kcal", menuImageName: "spongebob", menuDescription: "spongebob tidak untuk dimakan :)"),
-                    Menu(menuTitle: "patrick", menuCookTime: "30 Mins", menuCalorie: "123 kcal", menuImageName: "patrick", menuDescription: "patrick star berbentuk bintang, dengan saus mexico"),
-                    Menu(menuTitle: "squidward", menuCookTime: "10 Mins", menuCalorie: "111 kcal", menuImageName: "squidward", menuDescription: "digoreng dan diberikan bumbu khas bikini bottom")
-                ]),
-        OneDayMenu(dayNumber: 3,
-                   dayMenuList: [
-                    Menu(menuTitle: "Tempe Mendol", menuCookTime: "20 Mins", menuCalorie: "100 kcal", menuImageName: "tempe_mendol", menuDescription: "Deskripsi tempe mendol apa yaaa"),
-                    Menu(menuTitle: "Salted Egg Tempe", menuCookTime: "30 Mins", menuCalorie: "150 kcal", menuImageName: "tempe_salted_egg", menuDescription: "Wow salted egg tempe, enak sekali!!"),
-                    Menu(menuTitle: "Resep abc", menuCookTime: "30 Mins", menuCalorie: "150 kcal", menuImageName: "tempe_salted_egg", menuDescription: "tempe dengan kecap ABC ulala")
-                ]),
-        OneDayMenu(dayNumber: 4,
-                   dayMenuList: [
-                    Menu(menuTitle: "spongebob", menuCookTime: "20 Mins", menuCalorie: "100 kcal", menuImageName: "spongebob", menuDescription: "spongebob tidak untuk dimakan :)"),
-                    Menu(menuTitle: "patrick", menuCookTime: "30 Mins", menuCalorie: "123 kcal", menuImageName: "patrick", menuDescription: "patrick star berbentuk bintang, dengan saus mexico"),
-                    Menu(menuTitle: "squidward", menuCookTime: "10 Mins", menuCalorie: "111 kcal", menuImageName: "squidward", menuDescription: "digoreng dan diberikan bumbu khas bikini bottom")
-                ])
-    ]
-
+//    var weeklyRecipes: [OneDayMenu] = [
+//        OneDayMenu(dayNumber: 1,
+//                   dayMenuList: [
+//                    Menu(menuTitle: "Tempe Mendol", menuCookTime: "20 Mins", menuCalorie: "100 kcal", menuImageName: "tempe_mendol", menuDescription: "Deskripsi tempe mendol apa yaaa"),
+//                    Menu(menuTitle: "Salted Egg Tempe", menuCookTime: "30 Mins", menuCalorie: "150 kcal", menuImageName: "tempe_salted_egg", menuDescription: "Wow salted egg tempe, enak sekali!!"),
+//                    Menu(menuTitle: "Resep abc", menuCookTime: "30 Mins", menuCalorie: "150 kcal", menuImageName: "tempe_salted_egg", menuDescription: "tempe dengan kecap ABC ulala")
+//                ]),
+//        OneDayMenu(dayNumber: 2,
+//                   dayMenuList: [
+//                    Menu(menuTitle: "spongebob", menuCookTime: "20 Mins", menuCalorie: "100 kcal", menuImageName: "spongebob", menuDescription: "spongebob tidak untuk dimakan :)"),
+//                    Menu(menuTitle: "patrick", menuCookTime: "30 Mins", menuCalorie: "123 kcal", menuImageName: "patrick", menuDescription: "patrick star berbentuk bintang, dengan saus mexico"),
+//                    Menu(menuTitle: "squidward", menuCookTime: "10 Mins", menuCalorie: "111 kcal", menuImageName: "squidward", menuDescription: "digoreng dan diberikan bumbu khas bikini bottom")
+//                ]),
+//        OneDayMenu(dayNumber: 3,
+//                   dayMenuList: [
+//                    Menu(menuTitle: "Tempe Mendol", menuCookTime: "20 Mins", menuCalorie: "100 kcal", menuImageName: "tempe_mendol", menuDescription: "Deskripsi tempe mendol apa yaaa"),
+//                    Menu(menuTitle: "Salted Egg Tempe", menuCookTime: "30 Mins", menuCalorie: "150 kcal", menuImageName: "tempe_salted_egg", menuDescription: "Wow salted egg tempe, enak sekali!!"),
+//                    Menu(menuTitle: "Resep abc", menuCookTime: "30 Mins", menuCalorie: "150 kcal", menuImageName: "tempe_salted_egg", menuDescription: "tempe dengan kecap ABC ulala")
+//                ]),
+//        OneDayMenu(dayNumber: 4,
+//                   dayMenuList: [
+//                    Menu(menuTitle: "spongebob", menuCookTime: "20 Mins", menuCalorie: "100 kcal", menuImageName: "spongebob", menuDescription: "spongebob tidak untuk dimakan :)"),
+//                    Menu(menuTitle: "patrick", menuCookTime: "30 Mins", menuCalorie: "123 kcal", menuImageName: "patrick", menuDescription: "patrick star berbentuk bintang, dengan saus mexico"),
+//                    Menu(menuTitle: "squidward", menuCookTime: "10 Mins", menuCalorie: "111 kcal", menuImageName: "squidward", menuDescription: "digoreng dan diberikan bumbu khas bikini bottom")
+//                ])
+//    ]
     
     
     
@@ -113,7 +111,6 @@ class RecipeListViewController: ViewController {
     }
     /*
     // MARK: - Navigation
-
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
@@ -126,8 +123,7 @@ class RecipeListViewController: ViewController {
 extension RecipeListViewController: UITableViewDataSource {
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        return weeklyRecipes.count
-//        return 7
+        return 7
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -139,7 +135,6 @@ extension RecipeListViewController: UITableViewDataSource {
         let cell = AllRecipeTableView.dequeueReusableCell(withIdentifier: "RecipeCell") as! RecipeListTableViewCell
         
 //        let recipe = weeklyRecipes[indexPath.section].dayMenuList![indexPath.row]
-
 //        cell.RecipeNameLabel.text = recipe.menuTitle
         cell.RecipeNameLabel.text = recipeName[tableRow]
         cell.RecipeTimeCalorieLabel.text = "\(cookTime[tableRow]) Mins | \(calories[tableRow]) Kcal"
